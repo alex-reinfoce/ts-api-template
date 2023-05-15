72c03ae411f92a78c3af40a3ba33df6e65834547
Alex-Programer <alex.lichangnan@gmail.com>
2023-05-15 14:03:42 +0800

    feat: 测试覆盖

--- src/test/index.test.ts
+++ src/test/index.test.ts
@@ -1,7 +1,7 @@
 import { describe, it, expect } from 'vitest';
 import { isEmail, statusFactory } from '..';
 
-describe('render common api', () => {
+describe('test statusFactory', () => {
   it('render status', () => {
     const status = {
       1: 'yes',
@@ -15,6 +15,33 @@ describe('render common api', () => {
     expect(render('no')).toStrictEqual(2);
   });
 
+  it('render status with warning', () => {
+    let consoleWarnCalled = false;
+    const originalConsoleWarn = console.warn;
+
+    console.warn = () => {
+      consoleWarnCalled = true;
+    };
+
+    // 创建初始状态
+    const initStatus = {
+      A: 1,
+      B: 2,
+      C: 3,
+    };
+
+    // 创建statusFactory实例
+    const getStatus = statusFactory(initStatus);
+
+    // @ts-ignore
+    getStatus('E');
+    expect(consoleWarnCalled).toBe(true);
+
+    console.warn = originalConsoleWarn;
+  });
+});
+
+describe('test isEmail', () => {
   it('emial check', () => {
     expect(isEmail('123')).toBeFalsy();
     expect(isEmail('test@gmail.com')).toBeTruthy();
